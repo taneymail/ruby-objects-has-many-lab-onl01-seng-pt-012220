187550d9e33bce5b95dee3e56ddb92dedaac0882
T S <taneymail@gmail.com>
2020-02-22 18:09:03 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+class Song
+  
+  @@all = []
+  
+  attr_accessor :artist, :name
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def artist_name
+    if @artist == nil
+      return nil
+    else
+    self.artist.name
+  end
+  end
+end
