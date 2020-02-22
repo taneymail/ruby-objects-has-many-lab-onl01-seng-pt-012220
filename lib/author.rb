520027f82f6f790750b365464d4d578e1300670d
T S <taneymail@gmail.com>
2020-02-22 18:09:52 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,21 @@
+class Author 
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+  def posts
+    Post.all.select{|post| post.author == self}
+  end
+  def add_post(post)
+    post.author = self
+  end
+  def add_post_by_title(title)
+    new = Post.new(title)
+    add_post(new)
+  end
+  def self.post_count
+    Post.all.count
+  end
+end
